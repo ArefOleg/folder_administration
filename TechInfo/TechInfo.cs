0b550bad69c33fa367ac26ac14ea45afd13896a5
ArefOleg <serd-075@mail.ru>
2024-10-24 10:47:20 +0300

    update

--- TechInfo/TechInfo.cs
+++ TechInfo/TechInfo.cs
@@ -3,10 +3,11 @@ public static class TechInfo{
         Console.WriteLine("1 - Информация о АДМ на DEV\n2 - Доступы к средам\n3 - Информация для апплая");
         int techChoise = Convert.ToInt32(Console.ReadLine());
         switch(techChoise){
-            case 1:{
+            case 1:
 
+                break;            
+            case 2:
                 break;
-            }
             case 3:
                 Console.WriteLine("Table space DATA01\nIndex space INDEX01\nSIEBEL/WseJ3oid");     
             break;
