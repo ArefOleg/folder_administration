4795be97bb9e612b00d36d92e81e2b34f19e4d87
ArefOleg <serd-075@mail.ru>
2024-10-28 14:10:51 +0300

    update

--- folder_administration/folder_menu.cs
+++ folder_administration/folder_menu.cs
@@ -7,16 +7,23 @@ public static class File_Folder{
         DirectoryInfo dirInfo = new DirectoryInfo(path + @"\" + name);
         dirInfo.Create();
         Console.WriteLine("1 Создать минимальный лист поставки\n" +
-        "2  Лист поставки с изменениями аттрибутов\n");
+        "2  Лист поставки с изменениями аттрибутов\n"+
+        "3  Лист поставки минимальный с АДМ\n" +
+        "4  Лист поставки с изменениями аттрибутов и АДМ\n");
         int sqlChoise = Convert.ToInt32(Console.ReadLine());
         switch(sqlChoise){
                 case 1:
                         File.Copy(@"Word Template/ЛП минимальный.docx", path + @"\" + name + @"\" + name + ".doc");
                 break;
                 case 2:
-                        //     
+                        File.Copy(@"Word Template/ЛП с аттрибутами.docx", path + @"\" + name + @"\" + name + ".doc");     
                 break;
-                        
+                case 3:
+                        File.Copy(@"Word Template/ЛП минимальный c АДМ.docx", path + @"\" + name + @"\" + name + ".doc");     
+                break;
+                case 4:
+                        File.Copy(@"Word Template/ЛП с аттрибутами с АДМ.docx", path + @"\" + name + @"\" + name + ".doc");     
+                break;   
                 default:
                 break;
         }   
